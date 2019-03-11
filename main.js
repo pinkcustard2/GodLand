20145ec1c7decb4967eed14bbf89589431ae3b98
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2019-03-11 19:33:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var earthlyNews = [
 	"Sometimes I wonder why I am worshipping this god.",
 	"I walked through a town and they cheered 'The idiot has returned'. I looked around, but I couldn't see him.",
 ]
-document.write("<h1>God Land</h1>");
-document.write("<h2>-Earthly News-</h2>");
+
 var randomNews = function(){
 	var randomNumber = Math.floor(Math.random()*2);
 	
