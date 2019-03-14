2a84c15b6acddfb8e4398248c033d615da49cc5a
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2019-03-14 15:23:29 +0000

    Added new phrases

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 var earthlyNews = [
 	"Sometimes I wonder why I am worshipping this god.",
-	"I walked through a town and they cheered 'The idiot has returned'. I looked around, but I couldn't see him.",
+	"I walked past a person and he cheered 'The idiot has returned'. I looked around, but I couldn't see him.",
+	"Attempted to take the high road. Fell.",
+	"Saw some youths repeating my prayers in a high-pitched, childish voice and laughing. See, Almighty - news of your magnificence is spreading, and it's bringing people joy!",
+	"A stranger walked up to me and said 'My father told me not to talk to strangers'",
+	"I asked a man what his favourite thing was and he replied heavy metal. I dropped some heavy metal next to him. I wonder why he was so cross?",
 ]
-
+var max = 10
+var min = 120
+var setUp = function() {
+	document.write("<h1>God Land</h1>");
+	document.write("<h2>-Earthly News-</h2>");
+}
 var randomNews = function(){
-	var randomNumber = Math.floor(Math.random()*2);
+	var randomNumber = Math.floor(Math.random()*6);
 	
 	document.write(earthlyNews[randomNumber] + "<br>");
 
 	
 }
 
-
-setInterval(randomNews, 10000);
+setTimeout(setUp, 100);
+setInterval(randomNews, Math.floor(Math.random()*180000));
 
 
