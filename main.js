c77bf83e4d377e1d8b053c504e83e81ef0bcc8b3
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2019-03-14 18:12:52 +0000

    Added levels and favicons

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ var earthlyNews = [
 	"A stranger walked up to me and said 'My father told me not to talk to strangers'",
 	"I asked a man what his favourite thing was and he replied heavy metal. I dropped some heavy metal next to him. I wonder why he was so cross?",
 ]
-var max = 10
-var min = 120
+var max = 10;
+var min = 120;
+var exp = 0;
+var level = 1;
 var setUp = function() {
 	document.write("<h1>God Land</h1>");
+	document.write("Your current level is: " + level + "<br>");
+	document.write("Your current exp is: " + exp + "<br>");
 	document.write("<h2>-Earthly News-</h2>");
 }
 var randomNews = function(){
@@ -17,8 +21,16 @@ var randomNews = function(){
 	let d = new Date();
     let hours = d.getHours();
     let mins = d.getMinutes();
-	var secs = d.getSeconds();
+	let secs = d.getSeconds();;
+	exp = exp + Math.floor(Math.random()*100);
 	document.write("<b>"+hours+":"+mins+":"+secs+"  "+"</b>"+earthlyNews[randomNumber] + "<br>");
+	if (exp >= level * 100 + level){
+		exp = 0 
+		level++
+		document.write("You leveled up. Your current level is: " + level + "<br>");
+	}	else{
+		document.write("You earnt some exp. Your current exp is: " + exp + "<br>");
+	}
 }
 setTimeout(setUp, 100);
 setInterval(randomNews, Math.floor(Math.random()*180000));
