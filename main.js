ef3b40977a1bb95823d1af38509bf1a1d9b51c85
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2019-03-15 16:49:31 +0000

    Page now resets on level up

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var earthlyNews = [
-	"Sometimes I wonder why I am worshipping this god.",
+	"Sometimes I wonder why I am worshipping this god. Oh! I hope Almighty didn't hear that!",
 	"I walked past a person and he cheered 'The idiot has returned'. I looked around, but I couldn't see him.",
 	"Attempted to take the high road. Fell.",
 	"Saw some youths repeating my prayers in a high-pitched, childish voice and laughing. See, Almighty - news of your magnificence is spreading, and it's bringing people joy!",
@@ -13,7 +13,6 @@ var level = 1;
 var setUp = function() {
 	document.write("<h1>God Land</h1>");
 	document.write("Your current level is: " + level + "<br>");
-	document.write("Your current exp is: " + exp + "<br>");
 	document.write("<h2>-Earthly News-</h2>");
 }
 var randomNews = function(){
@@ -28,9 +27,15 @@ var randomNews = function(){
 		exp = 0 
 		level++
 		document.write("You leveled up. Your current level is: " + level + "<br>");
+		setTimeout(levelUp,1000)
 	}	else{
 		document.write("You earnt some exp. Your current exp is: " + exp + "<br>");
 	}
+}
+var levelUp = function(){
+	document.write("<h1>God Land</h1>");
+	document.write("Your current level is: " + "<div id=lvl>"+ level + "</div>"+"<br>");
+	document.write("<h2>-Earthly News-</h2>");
 }
 setTimeout(setUp, 100);
 setInterval(randomNews, Math.floor(Math.random()*180000));
